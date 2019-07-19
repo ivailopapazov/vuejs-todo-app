eef5c5d3c4f62ce675f1120a06fa8784d63ac043
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2019-07-19 19:32:50 +0300

    Add initial structure and files

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>
+      <label for="new-task">Add Item</label>
+      <input id="new-task" type="text" />
+      <button>Add</button>
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'add-todo'
+}
+</script>
+
+<style>
+
+</style>
